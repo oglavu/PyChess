112633dac68c2d3c82d6e3ac1925d50bb50b6f86
Ognjen Mirkovic <ognjenmirkovic032@gmail.com>
2023-10-24 01:17:22 +0200

    Comment figure.py

--- figure.py
+++ figure.py
@@ -1,3 +1,8 @@
+"""
+File containing class for each chess pieces.
+Every particular piece class inherits from Figure.
+"""
+
 import pygame as pg
 import consts
 
@@ -15,6 +20,7 @@ class Figure:
         self.lastMove = self.tile
         self.value = 0
 
+    # overloading == operator
     def __eq__(self, other):
         return self.tile == other.tile and self.color == other.color
     
@@ -27,20 +33,24 @@ class Figure:
     def draw(self, screen : pg.Surface):
         screen.blit(self.image, self.get_image_pos())
 
+    # return pos to blit image onto
     def get_image_pos(self):
         x, y= self.get_pos()
         return x + (consts.SIDE - self.image.get_width()) // 2, y + (consts.SIDE - self.image.get_width()) // 2
 
+    # based on tile, return (x, y) of a piece
     def get_pos(self, tile = ""):
         if tile:
             return ((ord(tile[0]) - ord("a")) * consts.SIDE, (8 - int(tile[1])) * consts.SIDE)
         return ((self.letter_ord - ord("a")) * consts.SIDE, (8 - self.num) * consts.SIDE)
-        
+
+    # returns rect dot cords (A, B, C, D) of tile
     def get_rect(self, tile = ""):
         if tile:
             return (x := (ord(tile[0]) - ord("a")) * consts.SIDE), (y := (8 - int(tile[1])) * consts.SIDE), x + consts.SIDE, y + consts.SIDE
         return (x := (self.letter_ord - ord("a")) * consts.SIDE), (y := (8 - self.num) * consts.SIDE), x + consts.SIDE, y + consts.SIDE
 
+    # draws transparent circle on a walkable tile
     def draw_circle(self, screen: pg.Surface, tile):
         x, y = self.get_pos(tile)
         plate = pg.Surface((consts.SIDE, consts.SIDE))
@@ -50,10 +60,12 @@ class Figure:
         pg.draw.circle(plate, consts.CIRCLE_COLOR, (rel_x, rel_y), consts.RADIUS)
         screen.blit(plate, (x, y))
 
+    # draws transparent circles on all walkable tiles
     def draw_to_go(self, screen: pg.Surface):
         for tile in self.where_to_go:
             self.draw_circle(screen, tile)
 
+    # if the piece is eaten, draw it scaled on a proper position
     def draw_eaten(self, screen: pg.Surface, order: int):
         img = pg.transform.scale(self.image, (consts.EATEN_SIZE, consts.EATEN_SIZE))
         x = consts.MARGIN + order * img.get_width() 
@@ -63,6 +75,7 @@ class Figure:
             y = consts.HEIGHT + 3 * consts.HEADER // 2 - img.get_height() // 2
         screen.blit(img, (x, y))
 
+    # yellow highlight for previous move 
     def show_previous_move(self, screen: pg.Surface):
         plate = pg.Surface((consts.SIDE, consts.SIDE))
         plate.set_colorkey(consts.BLACK)
@@ -70,7 +83,8 @@ class Figure:
         plate.fill(consts.LAST_MOVE_COLOR)
         screen.blit(plate, self.get_pos())
         screen.blit(plate, self.get_pos(self.lastMove))
-            
+
+    # checks if mouse over figure's tile   
     def isOverFig(self, pos) -> bool:
         x, y = pos
         x -= consts.MARGIN
@@ -79,6 +93,7 @@ class Figure:
         if selfX <= x <= selfX_ and selfY <= y <= selfY_:
             return True
 
+    # checks if mouse is over walkable tile
     def isOverCircle(self, tile, pos) -> bool:
         x, y = pos
         x -= consts.MARGIN
@@ -87,6 +102,7 @@ class Figure:
         if tileX <= x <=tileX_ and tileY <= y <= tileY_:
             return True
 
+    # moving piece
     def move(self, tile, figures = None):
         lastTile = self.tile
         self.__init__(tile, self.color)
@@ -97,13 +113,14 @@ class Figure:
 class King(Figure):
     def __init__(self, tile, color):
         super().__init__(tile, color)
-        self.image = pg.transform.scale2x(pg.image.load(f"images/KING_{self.color.upper()}.png"))
+        self.image = pg.transform.scale2x(pg.image.load(f"images/KING_{self.color.upper()}.png").convert_alpha())
         self.where_to_go = self.moving_pattern()
         self.symb = "K"
         self.checked = False
         self.mated = False
 
-    def draw(self, screen: pg.Surface): 
+    def draw(self, screen: pg.Surface):
+        # if king is checked draw red background behind him 
         if self.checked:
             x, y, _, _ = self.get_rect()
             plate = pg.Surface((consts.SIDE, consts.SIDE))
@@ -113,11 +130,15 @@ class King(Figure):
             screen.blit(plate, (x, y))
         screen.blit(self.image, self.get_image_pos())
 
+    # king's moving pattern
     def moving_pattern(self):
+        # neighbouring colums
         letters = [chr(letter) for letter in range(self.letter_ord - 1, self.letter_ord + 2) if ord("a") <= letter <= ord("h")]
+        # neighbouring rows
         nums = [num for num in range(self.num - 1, self.num + 2) if 1 <= num <= 8]
         return [tile for letter in letters for num in nums if (tile := f"{letter}{num}") != self.tile]
     
+    # removes taken tiles from king's moving pattern
     def block_tiles(self, taken_tiles):
         self.where_to_go = self.moving_pattern()
         new = self.where_to_go.copy()
@@ -127,6 +148,7 @@ class King(Figure):
                 new.remove(tile)
         self.where_to_go = new.copy()
 
+    # adds tiles where castelling is possible
     def ableToCastle(self, rooks, attacked, taken_tiles):
         for rook in rooks:
             if not(rook.moved or self.moved):
@@ -134,7 +156,8 @@ class King(Figure):
                 if not "g1" in attacked and rook.tile == "h1" and not "f1" in taken_tiles.keys() and not "g1" in taken_tiles.keys(): self.where_to_go.append("g1")
                 if not "c8" in attacked and rook.tile == "a8" and not "b8" in taken_tiles.keys() and not "c8" in taken_tiles.keys() and not "d8" in taken_tiles.keys(): self.where_to_go.append("c8")
                 if not "g8" in attacked and rook.tile == "h8" and not "f8" in taken_tiles.keys() and not "g8" in taken_tiles.keys(): self.where_to_go.append("g8")
-        
+    
+    # remove attacked tiles from king's moving pattern
     def forbidden_tiles(self, attacked, defended):
         new = self.where_to_go.copy()
         for tile in self.where_to_go:
@@ -163,7 +186,7 @@ class King(Figure):
 class Bishop(Figure):
     def __init__(self, tile, color):
         super().__init__(tile, color)
-        self.image = pg.transform.scale2x(pg.image.load(f"images/Bishop_{self.color.upper()}.png"))
+        self.image = pg.transform.scale2x(pg.image.load(f"images/Bishop_{self.color.upper()}.png").convert_alpha())
         self.where_to_go = self.moving_pattern()
         self.symb = "B"
         self.value = 3
@@ -198,6 +221,7 @@ class Bishop(Figure):
                 return self.bishop_boa(anchor, self.tile, rate)
             return "after"
 
+    # checks if piece is defended by this bishop
     def clean_defending(self):
         new = self.defending.copy()
         for tile in self.defending:
@@ -207,7 +231,8 @@ class Bishop(Figure):
             new = [tile_ for tile_ in new if (rate != self.rate(tile_)) or (bishop_boa == self.bishop_boa(tile, tile_, rate))]
 
         self.defending = new.copy()
-            
+
+    # removes tiles blocked by ally pieces from walkable tiles    
     def block_tiles(self, taken_tiles):
         self.defending = []
         self.where_to_go = self.moving_pattern()
@@ -231,7 +256,7 @@ class Bishop(Figure):
 class Rook(Figure):
     def __init__(self, tile, color):
         super().__init__(tile, color)
-        self.image = pg.transform.scale2x(pg.image.load(f"images/Rook_{self.color.upper()}.png"))
+        self.image = pg.transform.scale2x(pg.image.load(f"images/Rook_{self.color.upper()}.png").convert_alpha())
         self.where_to_go = self.moving_pattern()
         self.symb = "R"
         self.moved = False
@@ -297,7 +322,7 @@ class Rook(Figure):
 class Knight(Figure):
     def __init__(self, tile, color):
         super().__init__(tile, color)
-        self.image = pg.transform.scale2x(pg.image.load(f"images/Knight_{self.color.upper()}.png"))
+        self.image = pg.transform.scale2x(pg.image.load(f"images/Knight_{self.color.upper()}.png").convert_alpha())
         self.where_to_go = self.moving_pattern()
         self.symb = "N"
         self.value = 3
@@ -325,7 +350,7 @@ class Knight(Figure):
 class Pawn(Figure):
     def __init__(self, tile, color):
         super().__init__(tile, color)
-        self.image = pg.transform.scale2x(pg.image.load(f"images/Pawn_{self.color.upper()}.png"))
+        self.image = pg.transform.scale2x(pg.image.load(f"images/Pawn_{self.color.upper()}.png").convert_alpha())
         self.where_to_go = self.moving_pattern()
         self.symb = ""
         self.value = 1
@@ -395,7 +420,7 @@ class Pawn(Figure):
 class Queen(Figure):
     def __init__(self, tile, color):
         super().__init__(tile, color)
-        self.image = pg.transform.scale2x(pg.image.load(f"images/Queen_{self.color.upper()}.png"))
+        self.image = pg.transform.scale2x(pg.image.load(f"images/Queen_{self.color.upper()}.png").convert_alpha())
         self.Rook = Rook(tile, color)
         self.Bishop = Bishop(tile, color)
         self.where_to_go = self.moving_pattern()
